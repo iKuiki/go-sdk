b38e5c4ffc4bb13d0fc94bfa2851e09a7a2d4473
Kuiki <kuiki@kuiki.cn>
2023-03-17 21:22:43 +0800

    七牛外链改为非v2版本

--- qiniu/download.go
+++ qiniu/download.go
@@ -17,11 +17,11 @@ type download interface {
 
 // 返回公开存储的指定文件URL
 func (q *qiniu) MakePublicURL(domain string, key string) (url string) {
-	return storage.MakePublicURLv2(domain, key)
+	return storage.MakePublicURL(domain, key)
 }
 
 // 获取私有存储的指定文件的URL
 // 仅在指定expire时间段内有效
 func (q *qiniu) MakePrivateURL(domain string, key string, expire time.Duration) (url string) {
-	return storage.MakePrivateURLv2(q.credentials, domain, key, time.Now().Add(expire).Unix())
+	return storage.MakePrivateURL(q.credentials, domain, key, time.Now().Add(expire).Unix())
 }
